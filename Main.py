a554e926b89351b1963d805648743ac6457340ea
URK21CO2011 <110015075+URK21CO2011@users.noreply.github.com>
2022-07-26 23:00:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,33 +6,33 @@ class Stack:
         self.top = -1
 
     def is_empty(self):
-        if (self.top == -1):
+        if (stack.top == -1):
             return True
         else:
             return False
 
     def is_full(self):
-        if (self.top == self.size -1 ):
+        if (stack.top == stack.size -1 ):
             return True
         else:
             return False
 
     def push(self, data):
         if not self.is_full():
-           self.top += 1
-           self.items.append(data)
+           stack.top += 1
+           stack.items.append(data)
 
     def pop(self):
         if not self.is_empty():
-            x = self.items.pop()
-            self.top -= 1
+            x = stack.items.pop()
+            stack.top -= 1
             return x
 
     def status(self):
-        print(self.item)
+        for i in range(0, len(stack.items)):
+             print(stack.items[i],end="\n")
         
-s = input("enter the char:")
-ob = Stack(s)
+
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
